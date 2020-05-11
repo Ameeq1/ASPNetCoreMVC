f9aaf589d660c61894741f27d68ebc0cea1f5145
g k <g k@DESKTOP-7ENFFOR>
2020-05-11 15:01:44 +0530

    Implement MiddleWare

--- AppDemo1Repo/AppDemo1Repo/Startup.cs
+++ AppDemo1Repo/AppDemo1Repo/Startup.cs
@@ -15,6 +15,7 @@ namespace AppDemo1Repo
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -25,10 +26,26 @@ namespace AppDemo1Repo
                 app.UseDeveloperExceptionPage();
             }
 
-            app.Run(async (context) =>
+            //app.UseRouting();
+
+            // Implement MiddleWare
+
+            app.Use(async (context, next) =>
             {
-                await context.Response.WriteAsync("Hello World Ameeq!");
+                await context.Response.WriteAsync("Test");
+
+                await next();
             });
+
+            app.Use(async (context, next) =>
+            {
+                await context.Response.WriteAsync("Test");
+            });
+
+            //app.Run(async (context) =>
+            //{
+            //    await context.Response.WriteAsync("Hello World!");
+            //});
         }
     }
 }
